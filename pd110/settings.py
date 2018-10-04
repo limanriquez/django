5f08aca0f57a0253a9fdd3a01b473e505d660a32
DESKTOP-HTO27P8\LASSER <limanriquez@hotmail.com>
2018-10-04 16:07:00 -0600

    Whitenoise al settings del proyecto

--- pd110/settings.py
+++ pd110/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'pd110.urls'
@@ -123,6 +124,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,"static"),
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 #AUTH_USER_MODEL='authentication.Account'
 
